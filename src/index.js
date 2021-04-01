1963ec02d9ac8e831991b35b3f8fe5f060058fe2
caitlin <caitlin.wink@gmail.com>
2021-04-01 16:32:27 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ fetch("https://dog.ceo/api/breeds/image/random/4")
 });
 }
 
-const imgDrop = document.querySelector('body.breed-dropdown')
+const imgContainer = document.querySelector('div.dog-image-container');
+const imgDrop = document.querySelector('body.breed-dropdown');
 
 
 document.addEventListener('DOMContentLoaded', init);
