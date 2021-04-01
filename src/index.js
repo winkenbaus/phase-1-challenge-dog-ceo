e45c4711fed2eaf779cddb06610554f6bbdc9d42
caitlin <caitlin.wink@gmail.com>
2021-04-01 16:26:13 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+const init = () => {
+
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+fetch("https://dog.ceo/api/breeds/image/random/4")
+.then(function(response) {
+  console.log(response);
+  return response.json();
+})
+.then(function(json) {
+  console.log(json);
+});
+}
+
+const imgDrop = document.querySelector(body.breed-dropdown)
+
+
+document.addEventListener('DOMContentLoaded', init);
