bdf31f96d6715597cd3001ea1cb2e1172737b4e7
caitlin <caitlin.wink@gmail.com>
2021-04-02 14:31:31 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const init = () => {
-console.log('%c HI', 'color: firebrick')
-dogImages();
-dogListAll();
+  console.log('%c HI', 'color: firebrick')
+  dogImages();
+  dogListAll();
+  breedMenu().addEventListener('change', onChange)
+};
+
+let breeds
+
 function dogImages() {
     fetch("https://dog.ceo/api/breeds/image/random/4")
     .then(function(response) {
@@ -10,7 +15,8 @@ function dogImages() {
     .then(function(dogData) {
       renderDogs(dogData);
     });
-    }
+};
+
 function renderDogs(dogData) {
     const imageURLs = dogData.message
     imageURLs.forEach(URL => {
@@ -18,28 +24,40 @@ function renderDogs(dogData) {
         img.src = URL
         imgContainer().appendChild(img)
     });
-    }
-const imgContainer = () => document.getElementById('dog-image-container');
+};
 
 function dogListAll() {
-fetch("https://dog.ceo/api/breeds/list/all")
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(dogListAll) {
-      renderList(dogListAll);
+    fetch("https://dog.ceo/api/breeds/list/all")
+    .then(response => response.json() )
+    .then(dogListAll => {
+      breeds = Object.keys(Array.from(dogListAll.message))
+      renderList(breeds)
     });
-    }  
-function renderList(dogListAll) {
-    const listDogArray = dogListAll.message
+};  
+
+function renderList(listDogArray) {
     listDogArray.forEach(doggo => {
-        let listItems = document.createElement('li').src
-        listItems.src = doggo
+        let listItems = document.createElement('li')
+        listItems.innerText = doggo
         listContainer().appendChild(listItems)
     });
-    }
-const listContainer = () => document.getElementById('dog-breeds');
-}
-document.addEventListener('DOMContentLoaded', init);
+    listContainer().addEventListener('click', changeColor)
+};
 
-// const imgDrop = document.querySelector('body.breed-dropdown');
+function changeColor(e) {
+  e.target.style.color='red'
+};
+
+function onChange(e) {
+  console.log(e.target) 
+  let letter = e.target.value
+  listContainer().innerHTML = ' '
+  const filteredBreeds = breeds.filter((breed, list) => breeds.lastIndexOf(breed) === list).sort((a, b) => a < b ? -1 : 1);
+  renderList(filteredBreeds)
+}
+    
+
+const breedMenu = () => document.getElementById('breed-dropdown')
+const imgContainer = () => document.getElementById('dog-image-container');
+const listContainer = () => document.getElementById('dog-breeds');
+document.addEventListener('DOMContentLoaded', init);
