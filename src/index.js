b9409fce8d90b417ef0b3fd5d8e3c4a5019976b6
caitlin <caitlin.wink@gmail.com>
2021-04-01 19:01:04 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,45 @@
 const init = () => {
-
 console.log('%c HI', 'color: firebrick')
+dogImages();
+dogListAll();
+function dogImages() {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(dogData) {
+      renderDogs(dogData);
+    });
+    }
+function renderDogs(dogData) {
+    const imageURLs = dogData.message
+    imageURLs.forEach(URL => {
+        let img = document.createElement('img')
+        img.src = URL
+        imgContainer().appendChild(img)
+    });
+    }
+const imgContainer = () => document.getElementById('dog-image-container');
 
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
-fetch("https://dog.ceo/api/breeds/image/random/4")
-.then(function(response) {
-  console.log(response);
-  return response.json();
-})
-.then(function(json) {
-  console.log(json);
-});
+function dogListAll() {
+fetch("https://dog.ceo/api/breeds/list/all")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(dogListAll) {
+      renderList(dogListAll);
+    });
+    }  
+function renderList(dogListAll) {
+    const listDogArray = dogListAll.message
+    listDogArray.forEach(doggo => {
+        let listItems = document.createElement('li').src
+        listItems.src = doggo
+        listContainer().appendChild(listItems)
+    });
+    }
+const listContainer = () => document.getElementById('dog-breeds');
 }
-
-const imgContainer = document.querySelector('div.dog-image-container');
-const imgDrop = document.querySelector('body.breed-dropdown');
-
-
 document.addEventListener('DOMContentLoaded', init);
+
+// const imgDrop = document.querySelector('body.breed-dropdown');
